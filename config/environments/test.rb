8dc41e4c1cdaba6224928cc43c87415ddcec8a31
Scott Albertson <scott@thoughtbot.com>
2014-08-05 22:35:09 -0700

    Use new hash syntax

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,7 +36,7 @@ Houndapp::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => ENV["HOST"] }
+  config.action_mailer.default_url_options = { host: ENV["HOST"] }
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
